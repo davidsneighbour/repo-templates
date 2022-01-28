f4c33094b7ae30c81e78f0e958bf190485eefd58
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-01-28 23:14:35 +0700

    chore: recent changes

--- bin/build-auto-cards.sh
+++ bin/build-auto-cards.sh
@@ -25,6 +25,7 @@ COMPONENTS=(\
   "assets unsplash" \
   "asylum" \
   "auditor" \
+  "bin" \
   "components" \
   "debug" \
   "dev server sample" \
@@ -65,17 +66,21 @@ for TERM in "${COMPONENTS[@]}"; do
   find_string='>NAME<'
   replace_string=">${TERM^^}<"
   sed -E "s/${find_string}/${replace_string}/" "${IMAGEPATH}"/template.svg > "${IMAGEPATH}"/temp.svg
-  inkscape "${IMAGEPATH}"/temp.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-light.png
+  inkscape "${IMAGEPATH}"/temp.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-light.png --export-area=0:320:1280:640
   optipng -strip all -quiet -o7 "${IMAGEPATH}"/cards/github-card-"${TERM,,}"-light.png
   cp "${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-light.png ../dnb-hugo-"${TERM// /-}"/.github/github-card-light.png
+  inkscape "${IMAGEPATH}"/temp.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-light.png
+  optipng -strip all -quiet -o7 "${IMAGEPATH}"/cards/github-card-"${TERM,,}"-light.png
 
   # create dark version
   find_string='style="display:none"'
   replace_string='style="display:block"'
   sed -E "s/${find_string}/${replace_string}/" "${IMAGEPATH}"/temp.svg > "${IMAGEPATH}"/temp2.svg
-  inkscape "${IMAGEPATH}"/temp2.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-dark.png
+  inkscape "${IMAGEPATH}"/temp2.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-dark.png --export-area=0:320:1280:640
   optipng -strip all -quiet -o7 "${IMAGEPATH}"/cards/github-card-"${TERM,,}"-dark.png
   cp "${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-dark.png ../dnb-hugo-"${TERM// /-}"/.github/github-card-dark.png
+  inkscape "${IMAGEPATH}"/temp2.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-dark.png
+  optipng -strip all -quiet -o7 "${IMAGEPATH}"/cards/github-card-"${TERM,,}"-dark.png
 
   # cleanup
   rm -f "${IMAGEPATH}"/temp.svg "${IMAGEPATH}"/temp2.svg
