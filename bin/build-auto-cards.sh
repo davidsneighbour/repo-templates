c1a896bcbfba6550cf50bde9276460b500003e76
Patrick Kollitsch <patrick@davids-neighbour.com>
2022-04-07 19:20:25 +0700

    chore: updates and repo-renames

--- bin/build-auto-cards.sh
+++ bin/build-auto-cards.sh
@@ -21,51 +21,53 @@ done
 IMAGEPATH="./assets/images/auto-headers"
 
 # list of elements to create
-COMPONENTS=(\
-  "assets unsplash" \
-  "asylum" \
-  "auditor" \
-  "bin" \
-  "components" \
-  "debug" \
-  "dev server sample" \
-  "errors" \
-  "exchangerates" \
-  "feeds" \
-  "functions" \
-  "giscus" \
-  "head" \
-  "hooks" \
-  "humans" \
-  "icons" \
-  "internals" \
-  "netlification" \
-  "opensearch" \
-  "pictures" \
-  "publisher" \
-  "pwa" \
-  "renderhooks" \
-  "robots" \
-  "schema" \
-  "search algolia" \
-  "security" \
-  "shortcodes" \
-  "sitemap" \
-  "social" \
-  "templates" \
-  "testcontent" \
-  "workflows" \
-  "youtube" \
+COMPONENTS=(
+  "assets unsplash"
+  "asylum"
+  "auditor"
+  "bin"
+  "components"
+  "debug"
+  "dev server sample"
+  "errors"
+  "exchangerates"
+  "feeds"
+  "functions"
+  "giscus"
+  "github"
+  "head"
+  "hooks"
+  "humans"
+  "icons"
+  "internals"
+  "matomo"
+  "netlification"
+  "opensearch"
+  "pictures"
+  "publisher"
+  "pwa"
+  "renderhooks"
+  "robots"
+  "schema"
+  "search algolia"
+  "security"
+  "shortcodes"
+  "sitemap"
+  "social"
+  "templates"
+  "testcontent"
+  "workflows"
+  "youtube"
 )
 
 # go through elements
 for TERM in "${COMPONENTS[@]}"; do
 
   # create light version
-  echo "$TERM";
+  echo "$TERM"
   find_string='>NAME<'
   replace_string=">${TERM^^}<"
-  sed -E "s/${find_string}/${replace_string}/" "${IMAGEPATH}"/template.svg > "${IMAGEPATH}"/temp.svg
+  sed -E "s/${find_string}/${replace_string}/" "${IMAGEPATH}"/template.svg >"${IMAGEPATH}"/temp.svg
   inkscape "${IMAGEPATH}"/temp.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-light.png --export-area=0:320:1280:640
   optipng -strip all -quiet -o7 "${IMAGEPATH}"/cards/github-card-"${TERM,,}"-light.png
   cp "${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-light.png ../dnb-hugo-"${TERM// /-}"/.github/github-card-light.png
@@ -75,7 +77,7 @@ for TERM in "${COMPONENTS[@]}"; do
   # create dark version
   find_string='style="display:none"'
   replace_string='style="display:block"'
-  sed -E "s/${find_string}/${replace_string}/" "${IMAGEPATH}"/temp.svg > "${IMAGEPATH}"/temp2.svg
+  sed -E "s/${find_string}/${replace_string}/" "${IMAGEPATH}"/temp.svg >"${IMAGEPATH}"/temp2.svg
   inkscape "${IMAGEPATH}"/temp2.svg --export-filename="${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-dark.png --export-area=0:320:1280:640
   optipng -strip all -quiet -o7 "${IMAGEPATH}"/cards/github-card-"${TERM,,}"-dark.png
   cp "${IMAGEPATH}"/cards/github-card-"${TERM// /-}"-dark.png ../dnb-hugo-"${TERM// /-}"/.github/github-card-dark.png
